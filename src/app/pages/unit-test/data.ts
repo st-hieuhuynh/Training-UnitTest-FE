265ff653c83f45a441cb85fba29a5afd1ca29651
st-hieuhuynh <hieu.huynh@supremetech.vn>
2023-04-17 12:55:32 +0700

    Rewrite test case for function increase number array check

--- src/app/pages/unit-test/data.ts
+++ src/app/pages/unit-test/data.ts
@@ -1,5 +1,13 @@
-export const isArrIncrease = (arr: number[]) => {
+import { createLogger } from 'redux-logger';
+
+export const isArrIncrease = (arr: any) => {
+  if (!arr || !Array.isArray(arr) || arr.length === 0) {
+    return false;
+  }
   for (let i = 0; i < arr.length - 1; i++){
+    if (typeof arr[i] !== 'number' || Number.isNaN(arr[i])) {
+      return false;
+    }
     if (arr[i+1] < arr[i]) {
       return false;
     }
--- src/app/pages/unit-test/isArrIncrease.test.ts
+++ src/app/pages/unit-test/isArrIncrease.test.ts
@@ -2,25 +2,97 @@ import { describe, expect, test } from '@jest/globals';
 import { isArrIncrease } from './data';
 
 describe('test function isArrayIncrease', () => {
-  test('array null', () => {
-    expect(isArrIncrease([])).toBeTruthy();
+
+  describe('input is not a array', () => {
+    test('input is null', () => {
+      expect(isArrIncrease(null)).toBe(false);
+    });
+    test('input is undefined', () => {
+      expect(isArrIncrease(undefined)).toBe(false);
+    });
+    test('input is NaN', () => {
+      expect(isArrIncrease(NaN)).toBe(false);
+    });
+    test('input is number', () => {
+      expect(isArrIncrease(1)).toBe(false);
+    });
+    test('input is string', () => {
+      expect(isArrIncrease('1')).toBe(false);
+    });
+    test('input is boolean', () => {
+      expect(isArrIncrease(true)).toBe(false);
+    });
+    test('input is object', () => {
+      expect(isArrIncrease({})).toBe(false);
+    });
+    test('input is function', () => {
+      expect(isArrIncrease(() => {})).toBe(false);
+    });
   });
-  test('array with element value decrease', () => {
-    expect(isArrIncrease([5, 4, 3, 2, 1])).toBeFalsy();
+
+  describe('input is array with non-number items', () => {
+    test('input is empty array', () => {
+      expect(isArrIncrease([])).toBe(false);
+    });
+    test('input have null item', () => {
+      expect(isArrIncrease([1, null, 7, 9, 11])).toBe(false);
+    });
+    test('input have undefined item', () => {
+      expect(isArrIncrease([1, undefined, 7, 9, 11])).toBe(false);
+    });
+    test('input have NaN item', () => {
+      expect(isArrIncrease([1, NaN, 7, 9, 11])).toBe(false);
+    });
+    test('input have string item', () => {
+      expect(isArrIncrease([1, 'abc', 7, 9, 11])).toBe(false);
+    });
+    test('input have number as string item', () => {
+      expect(isArrIncrease([1, '7', 9, 10, 99])).toBe(false);
+    });
+    test('input have boolean true item', () => {
+      expect(isArrIncrease([1, true, 5, 7, 9])).toBe(false);
+    });
+    test('input have boolean false item', () => {
+      expect(isArrIncrease([1, false, 5, 7, 9])).toBe(false);
+    });
+    test('input have object item', () => {
+      expect(isArrIncrease([1, {}, 7, 9, 11])).toBe(false);
+    });
+    test('input have function item', () => {
+      expect(isArrIncrease([1, () => {}, 7, 9, 11])).toBe(false);
+    });
+    test('input have array item', () => {
+      expect(isArrIncrease([1, [], 7, 9, 11])).toBe(false);
+    });
+    test('input have object and function item', () => {
+      expect(isArrIncrease([1, {}, () => {}, 7, 9, 11])).toBe(false);
+    });
   });
-  test('array with elements arranged large and small alternately ', () => {
-    expect(isArrIncrease([2, 3, 6, 5])).toBeFalsy();
-  });
-  test('array only one elements', () => {
-    expect(isArrIncrease([1])).toBeTruthy();
-  });
-  test('array of elements with the same value', () => {
-    expect(isArrIncrease([2, 2, 2, 2])).toBeTruthy();
-  });
-  test('array have one elements same value', () => {
-    expect(isArrIncrease([1, 2, 2, 3, 4, 4, 5])).toBeTruthy();
-  });
-  test('array with element value increase', () => {
-    expect(isArrIncrease([1, 2, 3, 4, 5])).toBeTruthy();
+
+  describe('input is number array', () => {
+    test('input is negative increase number', () => {
+      expect(isArrIncrease([-5, -3, -2, -1])).toBe(true);
+    });
+    test('input is negative decrease number', () => {
+      expect(isArrIncrease([-1, -2, -3, -5])).toBe(false);
+    });
+    test('input is positive increase number', () => {
+      expect(isArrIncrease([1, 2, 3, 5])).toBe(true);
+    });
+    test('input is positive decrease number', () => {
+      expect(isArrIncrease([5, 3, 2, 1])).toBe(false);
+    });
+    test('input is float increase number', () => {
+      expect(isArrIncrease([0.5, 3.5, 4.9, 5.1, 5.1000001])).toBe(true);
+    });
+    test('input is float decrease number', () => {
+      expect(isArrIncrease([5.1000001, 5.1, 3.5, 3, 2.5])).toBe(false);
+    });
+    test('input is have mixed increase number', () => {
+      expect(isArrIncrease([-0.5, 3, 5, 5.1, 5.1000001])).toBe(true);
+    });
+    test('input is have mixed decrease number', () => {
+      expect(isArrIncrease([5.1000001, 5.1, 3.5, 3, 2.5])).toBe(false);
+    });
   });
 });
